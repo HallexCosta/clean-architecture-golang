53965730d2f235bbe1b7ab3d03aa5455c6b323d2
HallexCosta <hallex.costa@hotmail.com>
2020-11-16 18:09:39 -0300

    Defining entity User

--- src/entities/User.go
+++ src/entities/User.go
@@ -1,9 +1,13 @@
 package entities
 
+import "time"
+
 // User ...
 type User struct {
-	ID       int
-	Name     string
-	Email    string
-	Password string
+	ID        int
+	Name      string
+	Email     string
+	Password  string
+	CreateAt  time.Time
+	UpdatedAt time.Time
 }
