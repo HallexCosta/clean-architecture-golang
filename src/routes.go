e74d8a3b6c26a776b9aff3726b653be1aa170f6f
HallexCosta <hallex.costa@hotmail.com>
2020-11-16 18:11:20 -0300

    Defining for create new user

--- src/routes.go
+++ src/routes.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	useCases "github.com/HallexCosta/clean-architecture-golang/src/useCases/CreateUser"
+	"github.com/gofiber/fiber/v2"
+)
+
 // Routes ...
 func Routes() {
+	app.Post("/users", func(context *fiber.Ctx) error {
+		return useCases.CreateUser(context)
+	})
 }
